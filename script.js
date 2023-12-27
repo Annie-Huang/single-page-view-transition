db2ec19f5342e86b0f8e456ed93db638b6e1f8e4
Annie Huang <annie.hn@gmail.com>
2023-12-27 17:53:34 +1100

    Add event listener for each button and trigger updateActiveButton function when it is clicked

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
 const filterList = document.querySelector('.filter');
 const filterButtons = filterList.querySelectorAll('.filter-btn');
 const conferences = document.querySelectorAll('.conference');
+
+filterButtons.forEach((button) => {
+  button.addEventListener('click', (e) => {
+    // change the active button
+    updateActiveButton(e.target);
+
+    // filter the list
+    // filterEvents();
+  });
+});
+
+function updateActiveButton(newButton) {
+  // find the previously active button
+  // & remove the active class from it
+  // add active class to our new button
+}
